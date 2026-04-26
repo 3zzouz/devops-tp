2afa1343434e2bb6577bf131eb34326efe4621da
root <root@DESKTOP-9N3AMDR.>
2026-04-26 21:11:06 +0100

    fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,32 +1,39 @@
-# terraform/main.tf
 terraform {
   required_providers {
-    kubernetes = { source = "hashicorp/kubernetes", version = "~> 2.25" }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.25"
+    }
   }
 }
+
 provider "kubernetes" {
   config_path    = "~/.kube/config"
   config_context = "kind-devops-tp"
 }
+
 resource "kubernetes_namespace" "app" {
-  metadata { name = "devops-tp" }
+  metadata {
+    name = "devops-tp"
+  }
 }
+
 resource "kubernetes_limit_range" "app" {
-  metadata { name = "limits"; namespace = kubernetes_namespace.app.metadata[0].name }
+  metadata {
+    name      = "limits"
+    namespace = kubernetes_namespace.app.metadata[0].name
+  }
   spec {
     limit {
       type = "Container"
-      default        = { cpu = "200m", memory = "128Mi" }
-      default_request= { cpu = "100m", memory = "64Mi" }
+      default = {
+        cpu    = "200m"
+        memory = "128Mi"
+      }
+      default_request = {
+        cpu    = "100m"
+        memory = "64Mi"
+      }
     }
   }
 }
-# Stage Jenkins – Terraform
-    stage('Terraform Init & Apply') {
-      steps {
-        dir('terraform') {
-          sh 'terraform init'
-          sh 'terraform apply -auto-approve'
-        }
-      }
-    }
